5b651abcbd9c64a9e41204586d7989297a8fc743
anashofiyakurniawati <shofiya@alterra.id>
2020-08-29 02:09:39 +0700

    add threepanels component

---
+++ src/components/ThreePanels.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="md:grid md:grid-cols-9 gap-5 mx-4 md:mx-10 lg:mx-40 py-3 md:py-5 lg:py-10">
+    <div class="md:col-span-3 my-10 md:my-0 flex justify-center items-end">
+      <div>
+        <div class="flex justify-center">
+          <img
+            class="lg:w-full w-32 flex content-center"
+            src="../assets/images/zendguard.png"
+            alt="zendguard"
+          />
+        </div>
+        <p class="font-body text-sm lg:text-base">PHP Zend Guard Loader</p>
+      </div>
+    </div>
+    <div class="md:col-span-3 my-10 md:my-0 flex justify-center items-end">
+      <div>
+        <div class="flex justify-center">
+          <img class="lg:w-full w-24" src="../assets/images/composer.png" alt="zendguard" />
+        </div>
+        <p class="font-body text-sm lg:text-base">PHP Composer</p>
+      </div>
+    </div>
+    <div class="md:col-span-3 my-10 md:my-0 flex justify-center items-end">
+      <div>
+        <div class="flex justify-center">
+          <img class="lg:w-full w-32" src="../assets/images/ioncube.png" alt="zendguard" />
+        </div>
+        <p class="font-body text-sm lg:text-base">PHP ionCube Loader</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThreePanels",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Banner />
+    <ThreePanels />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner";
+import ThreePanels from "../components/ThreePanels";
 export default {
   name: "Home",
   components: {
     Banner,
+    ThreePanels,
   },
 };
 </script>
