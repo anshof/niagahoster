6f669c1d19433694172a7686160975387e39980e
anashofiyakurniawati <shofiya@alterra.id>
2020-08-29 02:19:11 +0700

    add quotes component

---
+++ src/components/Quotes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-10 lg:mx-48 my-3 md:my-10 lg:my-16 font-body">
+    <p class="text-lg lg:text-4xl font-bold">Paket Hosting Singapura yang Tepat</p>
+    <p class="lg:text-3xl text-base">Diskon 40% + Domain dan SSL Gratis untuk Anda</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quotes",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Banner />
     <ThreePanels />
+    <Quotes />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner";
 import ThreePanels from "../components/ThreePanels";
+import Quotes from "../components/Quotes";
 export default {
   name: "Home",
   components: {
     Banner,
     ThreePanels,
+    Quotes,
   },
 };
 </script>
